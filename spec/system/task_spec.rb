41b074344c414187e7cbbcafc6e9c4005c633471
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-09-16 18:15:30 +0900

    Model Spec基礎作成

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
-RSpec.describe 'タスク管理機能', type: :system do
-  describe '新規作成機能' do
-    context 'タスクを新規作成した場合' do
-      it '作成したタスクが表示される' do
+describe 'タスクモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context 'タスクのタイトルが空の場合' do
+      it 'バリデーションにひっかる' do
+        task = Task.new(name: '', content: '失敗テスト')
+# 6行目がtitleではなくnameなのはtaskの中にtitleが未定義だから
+        expect(task).not_to be_valid
+      end
+    end
+　　context 'タスクの詳細が空の場合' do
+      it 'バリデーションにひっかかる' do
+        # ここに内容を記載する
+      end
+    end
+    context 'タスクのタイトルと詳細に内容が記載されている場合' do
+      it 'バリデーションが通る' do
+        # ここに内容を記載する
       end
     end
   end
-  describe '一覧表示機能' do
-    context '一覧画面に遷移した場合' do
-      it '作成済みのタスク一覧が表示される' do
-      end
-    end
-  end
-  describe '詳細表示機能' do
-    context '任意のタスク詳細画面に遷移した場合' do
-      it '該当タスクの内容が表示される' do
-      end
-    end
-  end
-end
