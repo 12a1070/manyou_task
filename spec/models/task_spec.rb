5a5c6c5fa4905b591de0726b01c602259d9075bd
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-09-28 17:00:02 +0900

    modelテスト終了

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,30 +3,30 @@ RSpec.describe Task, type: :model do
   describe 'タスクモデル機能', type: :model do
     describe '検索機能' do
     # 必要に応じて、テストデータの内容を変更して構わない
-      let!(:task) { FactoryBot.create(:task, name: 'test',status:'完了') }
-      let!(:second_task) { FactoryBot.create(:second_task, name: "terai",status:'完了') }
+      let!(:task) { FactoryBot.create(:task, name: 'test') }
+      let!(:second_task) { FactoryBot.create(:second_task, name: "terai") }
       context 'scopeメソッドでタイトルのあいまい検索をした場合' do
         it "検索キーワードを含むタスクが絞り込まれる" do
         # title_seachはscopeで提示したタイトル検索用メソッドである。メソッド名は任意で構わない。
-          expect(Task.search_name('test')).to include(name)
-          expect(Task.search_name('test')).not_to include(second_name)
+          expect(Task.search_name('test')).to include(task)
+          expect(Task.search_name('test')).not_to include(second_task)
           expect(Task.search_name('test').count).to eq 1
         end
       end
       context 'scopeメソッドでステータス検索をした場合' do
         it "ステータスに完全一致するタスクが絞り込まれる" do
             # ここに内容を記載する
-          expect(Task.search_status('完了')).to include(name)
-          expect(Task.search_status('未着手')).not_to include(name)
+          expect(Task.search_status('着手中')).to include(task)
+          expect(Task.search_status('未着手')).not_to include(task)
           expect(Task.search_status('着手中').count).to eq 1
         end
       end
       context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
         it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
             # ここに内容を記載する
-          expect(Task.search_name(test).search_status('着手中')).to include(task)
-          expect(Task.search_name(test).search_status('着手中')).not_to include(second_task)
-          expect(Task.search_name(trewq).search_status('完了').count).to eq 1
+          expect(Task.search_name('test').search_status('着手中')).to include(task)
+          expect(Task.search_name('test').search_status('着手中')).not_to include(second_task)
+          expect(Task.search_name('terai').search_status('未着手').count).to eq 1
         end
       end
     end
