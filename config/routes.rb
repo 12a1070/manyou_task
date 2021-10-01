c5ff365b81312f4fbc6b063a819a74509a934e01
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-10-01 11:55:35 +0900

    管理画面にはかならず /admin というURLを先頭につける

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,13 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'users/new'
-    get 'users/edit'
-    get 'users/show'
-    get 'users/index'
-  end
-
-  resources :users
+  # アドミン定義
   namespace :admin do
     resources :users
   end
 
-
-
-  get 'sessions/new'
   resources :tasks
   root 'tasks#index'
 
+  # ユーザーのルーティング
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
